e97dfc4fadfe9ce4188fdc8dfcd08408d90e8e5e
Natwar Patidar <natvarp6062@gmail.com>
2025-01-03 23:56:50 +0530

    Added AA icon to toggle toolbar

--- src/components/atoms/Editor/Editor.jsx
+++ src/components/atoms/Editor/Editor.jsx
@@ -2,18 +2,26 @@ import 'quill/dist/quill.snow.css';
 
 import Quill from 'quill';
 import { useEffect, useRef, useState } from 'react';
+import { PiTextAa } from 'react-icons/pi';
+
+import { Button } from '@/components/ui/button';
 
 export const Editor = ({ variant = 'create', onSubmit, onCancel, placeholder, disabled, defaultValue }) => {
 
-    const [text, setText] = useState('');
-    const [isToolbarVisible, setToolbarVisible] = useState(false);
+    const [isToolbarVisible, setIsToolbarVisible] = useState(false);
 
     const containerRef = useRef();
-    const submitRef = useRef();
-    const disableRef = useRef();
     const placeholderRef = useRef();
     const defaultValueRef = useRef();
     const quillRef = useRef();
+
+    function toggleToolbar() {
+        setIsToolbarVisible(!isToolbarVisible);
+        const toolbar = containerRef.current.querySelector('.ql-toolbar');
+        if(toolbar) {
+            toolbar.classList.toggle('hidden');
+        }
+    }
 
     useEffect(()=> {
         if(!containerRef.current) return;
@@ -63,6 +71,16 @@ export const Editor = ({ variant = 'create', onSubmit, onCancel, placeholder, di
         <div className="flex flex-col">
             <div className="flex flex-col border-slate-300 rounded-md overflow-hidden focus-within:shadow-sm focus-within:border-slate-400 bg-white">
                 <div className='h-full ql-custom' ref={containerRef} />
+                <div className='flex px-2 pb-2 z-[5]'>
+                    <Button 
+                        size='iconSm' 
+                        onClick={toggleToolbar}
+                        variant='ghost'
+                        disabled={false}
+                    >
+                        <PiTextAa className='size-4' />
+                    </Button>
+                </div>
             </div>
             <p className='p-2 text-[10px] text-muted-foreground flex justify-end'>
                 <strong>Shift + return </strong> &nbsp; to add a new line
